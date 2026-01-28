6dc8693606dab505dd489b92ebf5bdd23e0c7c7d
xalmanfaris <xalmanfarisp@gmail.com>
2026-01-28 10:00:36 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,39 @@
+-- Created by GitHub Copilot in SSMS - review carefully before executing
+
+CREATE OR ALTER PROCEDURE dbo.GetAllBookTitles
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT Title
+    FROM dbo.Book;
+END;
+GO
+
+CREATE OR ALTER PROCEDURE dbo.GetBooksByAuthor
+    @AuthorName VARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT b.Title
+    FROM dbo.Book AS b
+    INNER JOIN dbo.Author AS a
+        ON b.AuthorId = a.AuthorId
+    WHERE a.AuthorName = @AuthorName;
+END;
+GO
+
+CREATE OR ALTER FUNCTION dbo.fn_BookCountByAuthor (@AuthorName VARCHAR(100))
+RETURNS INT
+AS
+BEGIN
+    RETURN (
+        SELECT COUNT(*)
+        FROM dbo.Book AS b
+        INNER JOIN dbo.Author AS a
+            ON b.AuthorId = a.AuthorId
+        WHERE a.AuthorName = @AuthorName
+    );
+END;
+GO
